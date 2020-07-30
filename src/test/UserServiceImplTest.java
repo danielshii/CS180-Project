de8842bbfb0923c0aadb313382c2a134c8c51616
William Shi <wshi88@hotmail.com>
2020-07-29 21:08:25 -0700

    Added UserServiceImplTest class

---
+++ src/test/UserServiceImplTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.Test;
+
+class UserServiceImplTest {
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void storeInFile() {
+    }
+
+    @Test
+    void login() {
+    }
+}
